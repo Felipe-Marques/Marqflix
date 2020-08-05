bcea7dbdae67132666573c3b2a5494c1f221c2e0
Felipe-Marques <felipe.souzamarques@outlook.com>
2020-08-04 23:40:38 -0300

    Inserindo o react-router-dom na aplicação

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
 import { FooterBase } from './styles';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
-      </a>
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="MarqFlix logo" />
+      </Link>
       <p>
-        Orgulhosamente criado durante a
-        {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
-        </a>
+        Orgulhosamente criado durante a{' '}
+        <a href="https://www.alura.com.br/">Imersão React da Alura</a>
       </p>
     </FooterBase>
   );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home/App';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+//Desafio da aula.
+const Pagina404 = () => <div>Página não encontrada</div>;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+      {/* Será executado como padrão, ideal para página de 4040 */}
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
